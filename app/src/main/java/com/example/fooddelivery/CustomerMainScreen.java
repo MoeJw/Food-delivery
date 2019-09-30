d5624e87d9ecbdbafd5f1315d5f76d83a15a3217
sudqi <mohammdomar19@gmail.com>
2019-09-30 11:32:11 +0300

    make_Customer_Main_Screen_1

---
+++ app/src/main/java/com/example/fooddelivery/CustomerMainScreen.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_main_screen);
+    }
+}
--- app/src/main/java/com/example/fooddelivery/MainActivity.java
+++ app/src/main/java/com/example/fooddelivery/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.fooddelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button log_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        log_in=findViewById(R.id.log_in_button);
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                open_acvtvity();
+            }
+        });
+
+    }
+    void open_acvtvity(){
+        Intent i=new Intent(this,CustomerMainScreen.class);
+        startActivity(i);
     }
 }
